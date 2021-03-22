8da939ac5dcb0ddfbc92ab0f534cf426962af6e1
ArdaTavusbay <arda.tavusbay@ug.bilkent.edu.tr>
2021-03-22 19:06:48 +0300

    Sign in page components & stylings, 4 new components overall

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
+import "./sign-in.styles.scss";
+
+export default class SignIn extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (e) => {
+    const { value, name } = e.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email & password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label="email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label="password"
+            required
+          />
+          <Button type="submit">Sign in</Button>
+        </form>
+      </div>
+    );
+  }
+}
